b4b20275f5b2685f2b145f035503bc97fe72e4d5
vishvasvikani07 <128354703+vishvasvikani07@users.noreply.github.com>
2024-03-07 14:38:45 +0530

    Initial Commit

---
+++ GallaryApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GallaryApplication
+//
+//  Created by 2022M16 on 07/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
